153da768fae7a62d44e8969fde975c151f2039a3
eduzissimo <eduardo.alves14@hotmail.com>
2023-08-29 19:08:41 -0300

    Initial commit: comitão bomba, realizando requisitos 1 e 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Search from './components/Search';
+
 function App() {
   return (
-    <p>Trybetunes</p>
+    <Routes>
+      <Route path="/" element={ <Login /> } />
+      <Route path="/search" element={ <Search /> } />
+    </Routes>
   );
 }
 
